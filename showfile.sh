6b648594092f1038bbac418bfa8be2d4e942b17c
Afobul <128596562+Afobul@users.noreply.github.com>
2023-11-05 21:54:04 -0100

    Update showfile.sh

--- showfile.sh
+++ showfile.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-# Define o arquivo de entrada
 arquivo="texto.txt"
 
-# Verifica se o arquivo existe
-if [ -f "$arquivo" ]; then
-    # Utiliza o comando "grep" para encontrar e exibir as frases no terminal
+if [ -f "$arquivo" ]
+then
     grep -E "O dia estava lindo|O sol brilhava no céu|As aves cantavam" "$arquivo"
 else
     echo "O arquivo $arquivo não existe."
 fi
 
 
-# ficheiro="./texto.txt"
-# frases=( "o dia.." "o sol ..." "as aves...")
-# grep -o "${frases[0]" ./texto.txt}
-# grep -o "${frases[1]" ./texto.txt}
-# grep -o "${frases[2]" ./texto.txt}
