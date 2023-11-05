3534a723ef827fc94cda50d03f089a4a1f72c411
Afobul <128596562+Afobul@users.noreply.github.com>
2023-11-05 22:01:29 -0100

    Update showfile.sh

--- showfile.sh
+++ showfile.sh
@@ -5,8 +5,6 @@ arquivo="texto.txt"
 if [ -f "$arquivo" ]
 then
     grep -E "O dia estava lindo|O sol brilhava no céu|As aves cantavam" "$arquivo"
-else
-    echo "O arquivo $arquivo não existe."
 fi
 
 
